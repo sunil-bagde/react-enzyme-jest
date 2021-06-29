8be9d2fda6622356ef1da2ef99bf97a1cecc59f9
Sunil Bagde <bagdesunil786@gmail.com>
2021-06-30 01:10:05 +0530

    tests basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Form from './Form'
+import TodoList from './TodoList'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    on: false,
+    input: '',
+    mainColor: 'blue',
+    lifeCycle: ''
+  }
+  handleStrings(str) {
+    if (str === 'Hello World') return true
+    return false
+  }
+  componentDidMount() {
+   this.setState({ lifeCycle: 'componentDidMount' })
+  }
+  componentWillReceiveProps() {
+    this.setState({ lifeCycle: 'componentWillReceiveProps' })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <h3 className={this.state.mainColor}>Everyone is Welcome!</h3>
+        </header>
+        <Form />
+        <p className="App-intro">Hello World</p>
+        <p className='button-state'>{this.state.on ? 'Yes!' : 'No!'}</p>
+        <button onClick={() => this.setState({on: true})}>Click</button>
+        <h2>{this.state.input}</h2>
+        <input onChange={(e) => this.setState({input: e.currentTarget.value})} type='text' />
+        <p className='lifeCycle'>{this.state.lifeCycle}</p>*
+      </div>
+    )
+  }
 }
 
-export default App;
+export class Link extends Component {
+  render() {
+    return this.props.hide ? null : <a href={this.props.address}>Click</a>
+  }
+}
+
+export default App
---
+++ src/Form.test.js
@@ -0,0 +1,109 @@
+import React from 'react'
+import Form from './Form'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
+import api from './api'
+
+configure({ adapter: new Adapter() })
+
+function updateInput(wrapper, instance, newValue) {
+    const input = wrapper.find(instance)
+    input.simulate('change', {
+        currentTarget: { value: newValue }
+    })
+    return wrapper.find(instance)
+}
+function testId(name) {
+    return `[data-testid="${name}"]`
+}
+describe("<Form />", () => {
+    test("receive ", () => {
+        const wrapper = shallow(<Form />)
+        const promotionInput = wrapper.find(testId("checked"));
+        const checker = promotionInput.props().checked
+        expect(checker).toBe(true)
+    })
+
+    test("allows user to fill form", () => {
+
+        const wrapper = shallow(<Form />)
+
+        const nameInput = updateInput(wrapper, testId("name"), 'admin')
+
+        expect(nameInput.props().value).toBe('admin')
+
+        const emailInput = updateInput(wrapper, testId("email"), 'admin@gmail.com')
+        const numberInput = updateInput(wrapper, testId("number"), '8655062213')
+        wrapper.find(testId("checked")).simulate('click')
+
+        expect(nameInput.props().value).toBe('admin')
+        expect(emailInput.props().value).toBe('admin@gmail.com')
+        expect(numberInput.props().value).toBe('8655062213')
+        expect(wrapper.find(testId("checked")).props().checked).toBe(false)
+
+
+    })
+    test('matches saved snapshot', () => {
+        const wrapper = shallow(<Form />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+});
+
+
+
+
+
+
+
+
+
+/*
+const updateInput = (wrapper, instance, newValue) => {
+    const input = wrapper.find(instance)
+    input.simulate('change', {
+        currentTarget: {value: newValue}
+    })
+    return wrapper.find(instance)
+}
+
+
+
+
+describe('<Form />', () => {
+    // opted in by default
+    test('receive promotions default is true', () => {
+        const wrapper = shallow(<Form />)
+        const promotionInput = wrapper.find('[data-testid="checked"]')
+        expect(promotionInput.props().checked).toBe(true)
+    })
+    // actually input their information
+    test('allows user to fill out form', () => {
+        const wrapper = shallow(<Form />)
+        const nameInput = updateInput(wrapper, '[data-testid="name"]', 'Tyler')
+        const emailInput = updateInput(wrapper, '[data-testid="email"]', 'test@gmail.com')
+        const numberInput = updateInput(wrapper, '[data-testid="number"]', '8018882321')
+        wrapper.find('[data-testid="checked"]').simulate('click')
+
+        expect(nameInput.props().value).toBe('Tyler')
+        expect(emailInput.props().value).toBe('test@gmail.com')
+        expect(numberInput.props().value).toBe('8018882321')
+        expect(wrapper.find('[data-testid="checked"]').props().checked).toBe(false)
+    })
+    //submits the form, calls api
+    test('submits the form', () => {
+        jest.spyOn(api, 'addUser').mockImplementation(() => Promise.resolve({data: 'New User Added'}))
+        const wrapper = shallow(<Form />)
+        updateInput(wrapper, '[data-testid="name"]', 'Tyler')
+        updateInput(wrapper, '[data-testid="email"]', 'test@gmail.com')
+        updateInput(wrapper, '[data-testid="number"]', '8018882321')
+        wrapper.find('[data-testid="addUserForm"]').simulate('submit', {preventDefault: () => {}})
+
+        expect(api.addUser).toHaveBeenCalledWith('Tyler', 'test@gmail.com', '8018882321')
+    })
+    // matches snapshot
+    test('matches saved snapshot', () => {
+        const wrapper = shallow(<Form />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+})*/
